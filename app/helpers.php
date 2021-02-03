3d60e047aa743e832ece5c12c305a7bc37600738
loyalself <plafhtcc@163.com>
2021-02-03 15:28:57 +0800

    2.6 基础页面结构

---
+++ app/helpers.php
@@ -0,0 +1,10 @@
+<?php
+use Illuminate\Support\Facades\Route;
+
+/**
+ * 将当前请求的路由名称转换为 CSS 类名称，作用是允许我们针对某个页面做页面样式定制
+ * @return string|string[]|null
+ */
+function route_class() {
+    return str_replace('.', '-', Route::currentRouteName());
+}
