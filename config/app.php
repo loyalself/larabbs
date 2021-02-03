4900fcc07cb7461aebec034a757afbb69f3ef392
loyalself <plafhtcc@163.com>
2021-02-03 15:57:24 +0800

    3.1 用户认证脚手架生成

--- config/app.php
+++ config/app.php
@@ -174,7 +174,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        //3.1 引入中文包
+        Overtrue\LaravelLang\TranslationServiceProvider::class
     ],
 
     /*
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,34 @@
 <?php
+
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', 'PagesController@root')->name('root');  //2.6-首页
+/**
+ * 3.1 运行 php artisan make:auth后会生成
+ * Auth::routes() 和 Route::get('/home', 'HomeController@index')->name('home') 两个路由;
+ * + Auth::routes() 是 Laravel 的用户认证路由，可以在 vendor/laravel/framework/src/Illuminate/Routing/Router.php 中搜索关
+   键词 LoginController 即可找到定义的地方.
+   + 由于我们已经定义了主页路由,就将 Route::get('/home', 'HomeController@index')->name('home') 删掉
+ */
+Auth::routes();
+//Auth::routes()相当于一下 1234 4个部分的路由
+// 1.用户身份验证相关的路由
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+// 2.用户注册相关路由
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');
+
+//3.密码重置相关路由...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update'); //更改密码
+
+// 4.Email 认证相关路由
+Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
+Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
+Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
