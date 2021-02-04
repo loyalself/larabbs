7da11f09e1aa87232d48c827eeba390593715e46
loyalself <plafhtcc@163.com>
2021-02-04 10:49:42 +0800

    3.5 邮箱验证

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -12,7 +12,7 @@ class RegisterController extends Controller
 {
     /*
     |--------------------------------------------------------------------------
-    | Register Controller
+    | Register Controller  注册逻辑写在这个 trait 里
     |--------------------------------------------------------------------------
     |
     | This controller handles the registration of new users as well as their
