a8bafad6000b8cf08fd87e88d76f39614dbcdeb3
loyalself <plafhtcc@163.com>
2021-02-09 17:30:59 +0800

    上传头像

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+/**
+ * AppServiceProvider 是框架的核心，在 Laravel 启动时，会最先加载该文件
+ */
 class AppServiceProvider extends ServiceProvider
 {
     /**
