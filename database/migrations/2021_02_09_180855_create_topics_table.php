5e08704b16aad9181019a391c38d76eb8e687a19
loyalself <plafhtcc@163.com>
2021-02-09 18:37:05 +0800

    给topics数据表迁移文件添加字段注释

--- database/migrations/2021_02_09_180855_create_topics_table.php
+++ database/migrations/2021_02_09_180855_create_topics_table.php
@@ -2,23 +2,24 @@
 
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
 
-class CreateTopicsTable extends Migration 
+class CreateTopicsTable extends Migration
 {
 	public function up()
 	{
 		Schema::create('topics', function(Blueprint $table) {
             $table->increments('id');
-            $table->string('title')->index();
-            $table->text('body');
-            $table->integer('user_id')->unsigned()->index();
-            $table->integer('category_id')->unsigned()->index();
-            $table->integer('reply_count')->unsigned()->default(0);
-            $table->integer('view_count')->unsigned()->default(0);
-            $table->integer('last_reply_user_id')->unsigned()->default(0);
-            $table->integer('order')->unsigned()->default(0);
-            $table->text('excerpt')->nullable();
-            $table->string('slug')->nullable();
+            $table->string('title')->index()->comment('帖子标题');
+            $table->text('body')->comment('帖子内容');
+            $table->integer('user_id')->unsigned()->index()->comment('用户id');
+            $table->integer('category_id')->unsigned()->index()->comment('分类id');
+            $table->integer('reply_count')->unsigned()->default(0)->comment('回复数量');
+            $table->integer('view_count')->unsigned()->default(0)->comment('查看总数');
+            $table->integer('last_reply_user_id')->unsigned()->default(0)->comment('最后回复的用户 ID');
+            $table->integer('order')->unsigned()->default(0)->comment('排序');
+            $table->text('excerpt')->nullable()->comment('文章摘要，SEO 优化时使用');
+            $table->string('slug')->nullable()->comment('SEO 友好的 URI');
             $table->timestamps();
         });
 	}
