90709a7b575f784580f444a0d2d457eec029f1a0
loyalself <plafhtcc@163.com>
2021-02-09 16:49:10 +0800

    3.6-认证后的消息提醒

---
+++ app/Listeners/EmailVerified.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class EmailVerified
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  Registered  $event
+     * @return void
+     */
+    public function handle(Registered $event)
+    {
+        // 会话里闪存认证成功后的消息提醒
+        session()->flash('success', '邮箱验证成功 ^_^');
+    }
+}
