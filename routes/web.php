04dbbaa76c44215efcb762c4fc2083ebf72c6c6d
loyalself <plafhtcc@163.com>
2021-02-09 17:01:15 +0800

    4.1-用户个人页面原型

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,9 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('passw
 Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
 Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
 Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+//上面代码等同于以下三条:
+Route::get('/users/{user}', 'UsersController@show')->name('users.show');      //显示用户个人信息页面
+Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit'); //显示编辑个人资料页面
+Route::patch('/users/{user}', 'UsersController@update')->name('users.update'); //处理 edit 页面提交的更改
