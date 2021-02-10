2a77a79c830773f711a7d419d234164f523db410
loyalself <plafhtcc@163.com>
2021-02-10 09:25:20 +0800

    5.5-话题列表首页

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -2,7 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 class Topic extends Model
 {
-    protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+    protected $fillable = [
+        'title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'
+    ];
+
+    /**
+     * 一个话题属于一个分类,一对一
+     * @return BelongsTo
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    /**
+     * 一个话题拥有一个用户,一对一
+     * @return BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
