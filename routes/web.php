1ea4526757bbcd319978638d1d3ed7314d85711a
loyalself <plafhtcc@163.com>
2021-02-19 17:48:21 +0800

    9.5-首页话题列表

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,8 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', 'PagesController@root')->name('root');  //2.6-首页
+//Route::get('/', 'PagesController@root')->name('root');  //2.6-首页
+Route::get('/', 'TopicsController@index')->name('root'); //9.5 修改路由
 /**
  * 3.1 运行 php artisan make:auth后会生成
  * Auth::routes() 和 Route::get('/home', 'HomeController@index')->name('home') 两个路由;
