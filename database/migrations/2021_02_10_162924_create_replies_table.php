96b25fbd68e7e4d062b3c65b94bbb22bd550f4ea
loyalself <plafhtcc@163.com>
2021-02-19 17:18:36 +0800

    9.3-联动删除

--- database/migrations/2021_02_10_162924_create_replies_table.php
+++ database/migrations/2021_02_10_162924_create_replies_table.php
@@ -2,15 +2,16 @@
 
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
 
-class CreateRepliesTable extends Migration 
+class CreateRepliesTable extends Migration
 {
 	public function up()
 	{
 		Schema::create('replies', function(Blueprint $table) {
             $table->increments('id');
             $table->integer('topic_id')->unsigned()->default(0)->index();
-            $table->bigInteger('user_id')->unsigned()->default(0)->index();
+            $table->integer('user_id')->unsigned()->default(0)->index();
             $table->text('content');
             $table->timestamps();
         });
