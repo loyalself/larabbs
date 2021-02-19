61f9cc5f609d372b3414db1c896ba3f8c1b4fc9d
loyalself <plafhtcc@163.com>
2021-02-19 10:50:31 +0800

    7.3-用户可以发表评论

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -9,9 +9,25 @@ use App\Models\Reply;
 
 class ReplyObserver
 {
+    /**
+     * 当有新的评论(回复)时,评论数(回复数)加1
+     * @param Reply $reply
+     */
+    public function created(Reply $reply)
+    {
+        //$reply->topic->increment('reply_count', 1);
+        //改进:
+        $reply->topic->reply_count = $reply->topic->replies->count();
+        $reply->topic->save();
+    }
+
+    /**
+     * 过滤 xss 攻击
+     * @param Reply $reply
+     */
     public function creating(Reply $reply)
     {
-        //
+        //$reply->content = clean($reply->content, 'user_topic_body');
     }
 
     public function updating(Reply $reply)
