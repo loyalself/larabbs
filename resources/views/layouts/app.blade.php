cda1f3503440859d0774fdfba491d9a7ef60b410
loyalself <plafhtcc@163.com>
2021-02-19 14:13:35 +0800

    8.2-站点权限部署

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,11 @@
     </div>
     @include('layouts._footer')
 </div>
+
+@if (app()->isLocal())
+    @include('sudosu::user-selector')
+@endif
+
 <!-- Scripts -->
 <script src="{{ mix('js/app.js') }}"></script>
 @yield('scripts')
