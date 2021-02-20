d95a42d0769888d0d954b006c222631e6168d4e0
loyalself <plafhtcc@163.com>
2021-02-20 15:37:01 +0800

    3.5-节流处理防止攻击

--- config/api.php
+++ config/api.php
@@ -125,6 +125,24 @@ return [
     'debug' => env('API_DEBUG', false),
 
     /*
+     * 接口频率限制
+     */
+    'rate_limits' => [
+        // 访问频率限制，次数/分钟  一分钟调用 60 次
+        'access' => [
+            'expires' => env('RATE_LIMITS_EXPIRES', 1),
+            'limit'   => env('RATE_LIMITS', 60),
+        ],
+        // 登录相关，次数/分钟  1分钟调用 10 次
+        'sign' => [
+            'expires' => env('SIGN_RATE_LIMITS_EXPIRES', 1),
+            'limit'   => env('SIGN_RATE_LIMITS', 10),
+        ]
+    ],
+
+
+
+    /*
     |--------------------------------------------------------------------------
     | Generic Error Format
     |--------------------------------------------------------------------------
