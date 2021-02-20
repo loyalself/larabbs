a48fe4f891a90542c68fb9d14441e89f1d6e0c85
loyalself <plafhtcc@163.com>
2021-02-20 14:48:44 +0800

    3.3-手机注册验证码

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,16 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+$api = app('Dingo\Api\Routing\Router');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+/**
+ * 我们增加了一个参数 namespace ，使 v1 版本的路由都会指向 App\Http\Controllers\Api
+ */
+$api->version('v1', [
+    'namespace' => 'App\Http\Controllers\Api'
+], function($api) {
+    // 短信验证码
+    $api->post('verificationCodes', 'VerificationCodesController@store')
+        ->name('api.verificationCodes.store');
+
 });
